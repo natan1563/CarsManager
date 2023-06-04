2ed20cab70019eb90cab6fc42d2598ed10c12961
natan1563 <natromafe@gmail.com>
2023-06-04 15:17:17 -0300

    make find service

---
+++ src/modules/vehicle/services/FindVehicleService.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from "tsyringe";
+import { IVehicleRepository } from "../repository/IVehicleRepository";
+import { VehicleInterface } from "../models/VehicleInterface";
+import NotFoundException from "src/exceptions/NotFoundException";
+
+@injectable()
+export default class FindVehicleService {
+  constructor(
+    @inject('VehicleRepository')
+    private vehicleRepository: IVehicleRepository
+  ) {}
+
+  public async execute(id: number): Promise<VehicleInterface | null>{
+    const vehicle = await this.vehicleRepository.findById(id)
+
+    if (!vehicle) {
+      throw new NotFoundException('Could not found vehicle')
+    }
+
+    return vehicle;
+  }
+}
