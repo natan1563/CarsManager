e49a01cc3d44a910f59ce32662e14b3401895a77
natan1563 <natromafe@gmail.com>
2023-06-04 15:17:25 -0300

    make create service

--- src/modules/vehicle/services/CreateVehicleService.ts
+++ src/modules/vehicle/services/CreateVehicleService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { IVehicleRepository } from "../repository/IVehicleRepository";
 import { VehicleInterface } from "../models/VehicleInterface";
+import BadRequestException from "src/exceptions/BadRequestException";
 
 @injectable()
 export default class CreateVehicleService {
@@ -10,8 +11,31 @@ export default class CreateVehicleService {
   ) {}
 
   public async execute(vehicle: VehicleInterface): Promise<VehicleInterface> {
-    const customer = await this.vehicleRepository.create(vehicle)
+    switch (true) {
+      case await this.vehiclePlateExists(vehicle.placa):
+        throw new BadRequestException('Vehicle plate already registered')
 
-    return customer;
+      case await this.vehicleChassiExists(vehicle.chassi):
+        throw new BadRequestException('Vehicle chassi already registered')
+
+      case await this.vehicleReindeerExists(vehicle.renavam):
+        throw new BadRequestException('Vehicle reindeer already registered')
+    }
+
+    const currentVehicle = await this.vehicleRepository.create(vehicle)
+
+    return currentVehicle;
   }
+
+  private async vehiclePlateExists(plate: string): Promise<boolean> {
+    return !!(await this.vehicleRepository.findByPlate(plate))
+  }
+
+  private async vehicleChassiExists(chassi: string): Promise<boolean> {
+    return !!(await this.vehicleRepository.findByChassi(chassi))
+  } 
+
+  private async vehicleReindeerExists(renavam: string): Promise<boolean> {
+    return !!(await this.vehicleRepository.findByReindeer(renavam))
+  } 
 }
